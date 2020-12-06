b972625d5355d8bcdd28d60b967751c47f0064af
wanghanbing <wanghanbing0521@163.com>
2020-12-06 12:39:30 +0800

    adjust error file context

--- src/main/java/com/wanghanbing/learn/javastudy/JavastudyApplication.java
+++ src/main/java/com/wanghanbing/learn/javastudy/JavastudyApplication.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class gitJavastudyApplication {
+public class JavastudyApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(JavastudyApplication.class, args);
